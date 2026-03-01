80406d54911ab32320817931904704fce55c216e
doreen <doreen20080615@gmail.com>
2026-03-01 15:47:43 +0800

    新增：完成產品列表頁和單一產品頁

--- src/views/front/Products.jsx
+++ src/views/front/Products.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const API_BASE = import.meta.env.VITE_API_BASE;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
+// 產品列表頁
 function Products() {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate(); // 製作切換頁面
 
+  //取得產品資料
   useEffect(() => {
     const getProducts = async () => {
       try {
@@ -21,6 +25,29 @@ function Products() {
     };
     getProducts();
   }, []);
+
+  // 方法一使用state方式："查看更多"的按鈕功能API
+  // const handleView = async (id) => {
+  //   try {
+  //     const response = await axios.get(
+  //       `${API_BASE}/api/${API_PATH}/product/${id}`,
+  //     );
+  //     console.log(response.data.product);
+  //     navigate(`/product/${id}`, {
+  //       state: {
+  //         productData: response.data.product, //將取得的資料傳入下一頁
+  //       },
+  //     });
+  //   } catch (error) {
+  //     console.log(error.response);
+  //   }
+  // };
+
+  // 方法二：使用useParams
+  const handleView = async (id) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -39,7 +66,11 @@ function Products() {
                 <p className="card-text">
                   <small className="text-body-secondary">{product.unit}</small>
                 </p>
-                <button type="button" className="btn btn-primary">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => handleView(product.id)}
+                >
                   查看更多
                 </button>
               </div>
